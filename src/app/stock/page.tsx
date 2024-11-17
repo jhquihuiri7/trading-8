8abba013da3699b6ef808d55ea7d7e7fe80f07a3
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2024-11-16 22:39:27 -0600

    build completed

--- src/app/stock/page.tsx
+++ src/app/stock/page.tsx
@@ -4,16 +4,21 @@ import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import {Stock_info} from "@/components";
 
-export default function About() {
-
+function AboutContent(){
   const searchParams = useSearchParams();
   const tvwidgetsymbol: string = searchParams.get('tvwidgetsymbol') || "";
+  return (
+          <div className="w-full flex flex-col items-center mt-10">
+            <Stock_info stock={tvwidgetsymbol}/>
+          </div>
+  )
+}
+
+export default function About() {
 
     return (
-      <div className="w-full flex flex-col items-center mt-10">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Stock_info stock={tvwidgetsymbol} />
+      <Suspense fallback={<div>Loading...</div>}>
+          <AboutContent/>
       </Suspense>
-      </div>
     );
   }
