417a651f18b6973409c38647c3068c1b9adfb7cf
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2024-11-16 20:29:53 -0600

    items removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,6 @@ import {Landing} from "@/components";
 
 export default function Home() {
 
-  const items = [
-    'S&P 500: 3372.2 +0.38%',
-    'NASDAQ: 12000.5 -0.15%',
-    'Dow Jones: 29000.8 +0.45%',
-    'FTSE 100: 7300.0 +0.22%',
-    'DAX: 15500.1 -0.12%',
-  ];
-
   return (
     <div className="">
       <Landing></Landing>
