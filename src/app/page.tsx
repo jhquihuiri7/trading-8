517be1e98585b6f9b78c61fe4e4c650b88607e4f
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2024-11-18 13:11:22 -0600

    print error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,22 @@ import {Landing} from "@/components";
 
 export default async function Home() {
   const limit = 20;
-  const res = await fetch("https://api.binance.com/api/v3/ticker/24hr")
-  if (!res.ok) {
-    throw new Error("Error al obtener datos de Binance");
-  }
+  let data = [];
+  try{
 
-  const response = await res.json();
+    const res = await fetch("https://api.binance.com/api/v3/ticker/24hr")
 
-  const topCryptos = response
+    const response = await res.json();
+
+    const topCryptos = response
       .sort((a: any, b: any) => parseFloat(b.quoteVolume) - parseFloat(a.quoteVolume))
       .slice(0, limit);
 
-  const data = topCryptos.map((crypto: any) => crypto.symbol);
+    data = topCryptos.map((crypto: any) => crypto.symbol);
+
+  }catch(error){
+    console.log(error)
+  }
   
    return (
     <div>
