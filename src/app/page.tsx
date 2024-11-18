ddbab3affd46c5239ccb5ac8c6e23cb3495fd717
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2024-11-18 13:15:38 -0600

    print error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,17 @@ export default async function Home() {
     const res = await fetch("https://api.binance.com/api/v3/ticker/24hr")
 
     const response = await res.json();
-
+    console.log(response)
     const topCryptos = response
       .sort((a: any, b: any) => parseFloat(b.quoteVolume) - parseFloat(a.quoteVolume))
       .slice(0, limit);
-
+    console.log(topCryptos)
     data = topCryptos.map((crypto: any) => crypto.symbol);
-
+    console.log(data)
   }catch(error){
     console.log(error)
   }
-  
+  console.log(data)
    return (
     <div>
       <Landing items={data}></Landing>
