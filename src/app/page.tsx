2059f6f9ba91d3720aeaf54d93364a35effc89fe
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2024-11-18 14:19:45 -0600

    region issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import {Landing} from "@/components";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
+export const config = {
+  runtime: 'edge',  // This tells Next.js to use Edge runtime for this route
+  regions: ['hnd1'],  // Define the edge regions (iad1 = Northern Virginia, hnd1 = Tokyo)
+};
 
-
-export default async function Home() {
+export default async function Home(request: NextRequest) {
   const limit = 20;
   let data = [];
+  
   try{
 
     const res = await fetch("https://api.binance.com/api/v3/ticker/24hr")
@@ -22,6 +28,7 @@ export default async function Home() {
     console.log(error)
   }
   console.log(data)
+  console.log(process.env.VERCEL_REGION)
    return (
     <div>
       <Landing items={data}></Landing>
